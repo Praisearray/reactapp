55725d98a43805bba5d91c1014568f40535d1e2c
swapnilnavalakha <swapnil.navalakha@taranawireless.com>
2023-04-01 13:35:07 +0530

    test changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit 123 789 <code>src/App.js</code> and save to reload.
+          Hello <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
