b5f6f9a2d43ac077cc57c5a021f0e7141c546073
abhisheklandge77 <abhisheknlandge@gmail.com>
2023-05-31 22:19:58 +0530

    Added map

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const CONSTANTS = {
-    MAP_STR: "pk.eyJ1IjoiYWJoaXNoZWstbGFuZGdlIiwiYSI6ImNsaWFkdnNxdTAybngzc3M2ZGQ3MG11c2wifQ.qcfj4Y4STfc75UuQT2ZM7A",
+    MAP_STR: "pk.eyJ1IjoiYWJoaXNoZWstbGFuZGdlIiwiYSI6ImNsaWJ5MDRqMDAwN2czZWxqYnlteXIxMHEifQ.kPgVYh2vsHy1u-EAoCL-Og",
 }
